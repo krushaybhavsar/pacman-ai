c08673963ecd07b7b41743601c2ad07dfb7249fc
Krushay Bhavsar <krushaybhavsar@gmail.com>
2020-11-27 23:49:13 -0500

    Bug Fix

--- enemy_class.py
+++ enemy_class.py
@@ -12,7 +12,7 @@ class Enemy:
         self.radius = int(self.app.cell_width // 2.3)
         self.number = number
         self.color = self.set_color()
-        self.direction = vec(1, 0)
+        self.direction = vec(0, 0)
         self.personality = self.set_personality()
         self.target = None
         self.speed = self.set_speed()
@@ -54,10 +54,10 @@ class Enemy:
 
     def time_to_move(self):
         if int(self.pix_pos.x + TOP_BOTTOM_BUFFER//2) % self.app.cell_width == 0:
-            if self.direction == vec(1, 0) or self.direction == vec(-1, 0):
+            if self.direction == vec(1, 0) or self.direction == vec(-1, 0) or self.direction == vec(0, 0):
                 return True
         if int(self.pix_pos.y + TOP_BOTTOM_BUFFER//2) % self.app.cell_height == 0:
-            if self.direction == vec(0, 1) or self.direction == vec(0, -1):
+            if self.direction == vec(0, 1) or self.direction == vec(0, -1) or self.direction == vec(0, 0):
                 return True
         return False
 
